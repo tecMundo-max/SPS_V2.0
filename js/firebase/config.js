e106d2cd3df094ffd44eb8ae9b3cdc505d806630
tecMundo-max <techeliojunior@gmail.com>
2026-06-28 18:50:02 -0300

    Update Firebase config with new project settings
    Updated Firebase configuration with new project details.

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -16,19 +16,23 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-
  * Configuração do Firebase
  * Preencha com os dados do projeto SPS003HJR.
  */
-const firebaseConfig = Object.freeze({
-    apiKey: "SUA_API_KEY",
-    authDomain: "SPS003HJR.firebaseapp.com",
-    databaseURL: "https://SPS003HJR-default-rtdb.firebaseio.com",
-    projectId: "SPS003HJR",
-    storageBucket: "SPS003HJR.firebasestorage.app",
-    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-    appId: "SEU_APP_ID"
-});
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-/**
- * Instância única da aplicação Firebase
- */
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYnvcRKt4qDpk08_4MQVNEOr9Iq95X980",
+  authDomain: "sps003hjr.firebaseapp.com",
+  databaseURL: "https://sps003hjr-default-rtdb.firebaseio.com",
+  projectId: "sps003hjr",
+  storageBucket: "sps003hjr.firebasestorage.app",
+  messagingSenderId: "294133615625",
+  appId: "1:294133615625:web:832c4db972dcb1b92cbcd8"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 /**
