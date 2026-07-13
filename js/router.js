4582c1648d11a7d978e99d59c157032084f9ad5b
tecMundo-max <techeliojunior@gmail.com>
2026-07-13 16:19:48 -0300

    profile ? .ativo === true por profile?.ativo === true

--- js/router.js
+++ js/router.js
@@ -109,7 +109,7 @@ export async function initialize() {
     if (
         route.publicOnly &&
         isAuthenticated() &&
-        profile?.ativo === true
+      profile?.ativo === true
     ) {
         navigateToDefaultAuthenticatedPage();
 
