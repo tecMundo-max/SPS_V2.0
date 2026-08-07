3881a2e602b56c1ad90633b4e4a38e55aa9acf37
tecMundo-max <techeliojunior@gmail.com>
2026-08-07 18:26:31 -0300

    Refactor Firebase config and initialization

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -1,23 +1,19 @@
-// js/firebase/config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDYnvcRKt4qDpk08_4MQVNEOr9Iq95X980",
-  authDomain: "sps003hjr.firebaseapp.com",
-  databaseURL: "https://sps003hjr-default-rtdb.firebaseio.com",
-  projectId: "sps003hjr",
-  storageBucket: "sps003hjr.firebasestorage.app",
-  messagingSenderId: "294133615625",
-  appId: "1:294133615625:web:832c4db972dcb1b92cbcd8"
+    apiKey: "AIzaSyDYnvcRKt4qDpk08_4MQVNEOr9Iq95X980",
+    authDomain: "sps003hjr.firebaseapp.com",
+    databaseURL: "https://sps003hjr-default-rtdb.firebaseio.com",
+    projectId: "sps003hjr",
+    storageBucket: "sps003hjr.firebasestorage.app",
+    messagingSenderId: "294133615625",
+    appId: "1:294133615625:web:832c4db972dcb1b92cbcd8"
 };
 
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
+// Exporta serviços
+const auth = firebase.auth();
+const database = firebase.database();
 
-export { app, auth, db, firebaseConfig };
-export default app;
-
+export { auth, database };
