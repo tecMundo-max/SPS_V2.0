1952e9a4f267172ded11b4de8d4d0714034c8d71
tecMundo-max <techeliojunior@gmail.com>
2026-08-06 23:32:24 -0300

    Update config.js

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -4,14 +4,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAeCrURSs0TBXlYF3TKLi4q98VwrGaKe_Q",
-    authDomain: "spsch-849e5.firebaseapp.com",
-    databaseURL: "https://spsch-849e5-default-rtdb.firebaseio.com",
-    projectId: "spsch-849e5",
-    storageBucket: "spsch-849e5.firebasestorage.app",
-    messagingSenderId: "698967090558",
-    appId: "1:698967090558:web:978781fd27b86c36203f2f"
+  apiKey: "AIzaSyDYnvcRKt4qDpk08_4MQVNEOr9Iq95X980",
+  authDomain: "sps003hjr.firebaseapp.com",
+  databaseURL: "https://sps003hjr-default-rtdb.firebaseio.com",
+  projectId: "sps003hjr",
+  storageBucket: "sps003hjr.firebasestorage.app",
+  messagingSenderId: "294133615625",
+  appId: "1:294133615625:web:832c4db972dcb1b92cbcd8"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -19,3 +20,4 @@ const db = getDatabase(app);
 
 export { app, auth, db, firebaseConfig };
 export default app;
+
